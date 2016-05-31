60a8d09c54ddf69875f3aef3de592fa48347168e
Alexander Wallin <office@alexanderwallin.com>
2016-05-31 15:58:33 +0100

    Makes the build exported as CommonJS

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/',
-    filename: 'index.js'
+    filename: 'index.js',
+    libraryTarget: 'commonjs'
   },
   module: {
     loaders: [{
